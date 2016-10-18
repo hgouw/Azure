29ae445157060c863d8b3132f86f29301071a199
Herman Gouw <hermangouw@gmail.com>
2016-10-18 14:34:03 +1100

    Implemented SaveFiles to database

--- Azure.CopyFiles/Program.cs
+++ Azure.CopyFiles/Program.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.WindowsAzure;
 using Microsoft.WindowsAzure.Storage;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
+using System.Xml;
+using Local.DataAccess;
 
-namespace Azure.CopyFilesSample
+namespace Azure.CopyFiles
 {
     public class Program
     {
@@ -15,6 +18,8 @@ namespace Azure.CopyFilesSample
             {
                 CopyFilesAsync().Wait();
                 Console.WriteLine("Successfully copying files to Azure Blob Storage");
+                //SaveFiles();
+                //Console.WriteLine("Successfully saving files to SQL Server Database");
             }
             catch (FormatException)
             {
@@ -46,6 +51,27 @@ namespace Azure.CopyFilesSample
             {
                 var blockBlob = blobContainer.GetBlockBlobReference(Path.GetFileName(file));
                 await blockBlob.UploadFromFileAsync(file, FileMode.Open);
+            }
+        }
+
+        private static void SaveFiles()
+        {
+            var files = Directory.GetFiles(CloudConfigurationManager.GetSetting("DataFolder"));
+            Array.Sort(files, StringComparer.InvariantCulture);
+            using (var db = new AzureTestModelContainer())
+            {
+                foreach (var file in files)
+                {
+                    var doc = new XmlDocument();
+                    doc.Load(file);
+                    var employees = new List<Employee>();
+                    foreach (XmlNode node in doc.DocumentElement)
+                    {
+                        employees.Add(new Employee { Name = node["Name"].InnerText, Location = node["Location"].InnerText });
+                    }
+                    db.Employees.AddRange(employees);
+                    db.SaveChanges();
+                }
             }
         }
 
