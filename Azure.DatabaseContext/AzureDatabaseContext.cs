36d79da062ddc47e370fd70d0011ba0ae91a5643
Herman Gouw <hermangouw@gmail.com>
2016-10-20 11:55:52 +1100

    Added Azure.DatabaseContext project

---
+++ Azure.DatabaseContext/AzureDatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Azure.DatabaseContext
+{
+    public class AzureDatabaseContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        public static IList<Employee> GetEmployees()
+        {
+            using (AzureDatabaseContext dbContext = new AzureDatabaseContext())
+            {
+                return dbContext.Employees.ToList();
+            }
+        }
+    }
+}
---
+++ Azure.DatabaseContext/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace Azure.DatabaseContext
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+    }
+}
