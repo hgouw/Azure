4d80a119fddd0ec3276fc11cfa22bc1d61ffb940
Herman Gouw <hermangouw@gmail.com>
2016-10-21 12:09:23 +1100

    Implemented ParseFiles

--- Azure.ParseFiles/Program.cs
+++ Azure.ParseFiles/Program.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
-using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace Azure.ParseFiles
 {
@@ -10,24 +11,42 @@ namespace Azure.ParseFiles
     {
         static void Main()
         {
-            Console.WriteLine("Started parsing files");
-            ParseFiles();
-            Console.WriteLine("Successfully parsing files");
+            Console.WriteLine("Started parsing the files");
+            if (ParseFiles())
+                Console.WriteLine("Successfully parsing the files");
+            else
+                Console.WriteLine("Unsuccessful in parsing the files");
             Console.ReadLine();
         }
 
-        private static void ParseFiles()
-        {           
+        private static bool ParseFiles()
+        {
+            var ok = true;
             var files = Directory.GetFiles(ConfigurationManager.AppSettings["DataFolder"]);
             Array.Sort(files, StringComparer.InvariantCulture);
             var caseDocs = new List<CaseDoc>();
             foreach (var file in files)
             {
                 if (file.ToLower().EndsWith(".xml", StringComparison.CurrentCulture))
-                {
-                    var parser = new Parser(file);
-                    caseDocs.Add(parser.ParseXml());
-                }
+                    if (!ParseXml(file))
+                        ok = false;
+            }
+            return ok;
+        }
+
+        private static bool ParseXml(string file)
+        {
+            XmlReaderSettings settings = new XmlReaderSettings() { DtdProcessing = DtdProcessing.Parse };
+            XmlReader reader = XmlReader.Create(file, settings);
+            XDocument doc = null;
+            try
+            {
+                doc = XDocument.Load(reader);
+                return true;
+            }
+            catch
+            {
+                return false;
             }
         }
     }
