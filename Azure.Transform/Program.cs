3c3d9e7040e8958d2a23d3c871bb37cd6ebda069
Herman Gouw <hermangouw@gmail.com>
2016-11-21 15:45:43 +1100

    Implemented XslCompiledTransform.Transform to StringBuilder

--- Azure.Transform/Program.cs
+++ Azure.Transform/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Xml;
 using System.Xml.Xsl;
 
 namespace Azure.Transform
@@ -10,18 +13,26 @@ namespace Azure.Transform
         {
             var watch = Stopwatch.StartNew();
 
+            watch.Start();
             var transform = new XslTransform();
             transform.Load(@"F:\URJ\breakfast.xsl");
             transform.Transform(@"F:\URJ\breakfast.xml", @"F:\breakfast1.html");
-
             watch.Stop();
-            Console.WriteLine("XslTransform: " +  watch.ElapsedMilliseconds.ToString() + "ms");
+            Console.WriteLine("XslTransform: " + watch.ElapsedMilliseconds.ToString() + "ms");
 
+            watch.Start();
             var compiledTransform = new XslCompiledTransform();
             compiledTransform.Load(@"F:\URJ\breakfast.xsl");
             compiledTransform.Transform(@"F:\URJ\breakfast.xml", @"F:\breakfast2.html");
+            watch.Stop();
+            Console.WriteLine("XslCompiledTransform: " + watch.ElapsedMilliseconds.ToString() + "ms");
 
-            watch = Stopwatch.StartNew();
+            watch.Start();
+            compiledTransform.Load(@"F:\URJ\breakfast.xsl");
+            var sb = new StringBuilder();
+            var xmlWriter = XmlWriter.Create(sb);
+            compiledTransform.Transform(@"F:\URJ\breakfast.xml", xmlWriter);
+            File.WriteAllText(@"F:\breakfast3.html", sb.ToString());
             watch.Stop();
             Console.WriteLine("XslCompiledTransform: " + watch.ElapsedMilliseconds.ToString() + "ms");
 
