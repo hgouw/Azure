3d6121d41e487c94c9c152b9a19a4fea84bb6483
Herman Gouw <hermangouw@gmail.com>
2016-11-23 18:00:00 +1100

    Implemented PublishEvents and ReceiveEvents

--- Azure.PublishEvents/Program.cs
+++ Azure.PublishEvents/Program.cs
@@ -1,9 +1,28 @@
-﻿namespace Azure.PublishEvents
+﻿using System;
+using System.Text;
+using System.Threading;
+using Microsoft.ServiceBus.Messaging;
+
+namespace Azure.PublishEvents
 {
     class Program
     {
         static void Main()
         {
+            try
+            {
+                var factory = MessagingFactory.CreateFromConnectionString("Endpoint=sb://optus-test.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=XmnjtQXdULTRKNm0sv8rNHCjTH+j0Iejx8p1gntJP74=" + ";TransportType=Amqp");
+                var client = factory.CreateEventHubClient("optus");
+                for (int i = 0; i < 6; i++)
+                {
+                    var message = Guid.NewGuid().ToString();
+                    client.Send(new EventData(Encoding.UTF8.GetBytes(message)));
+                    Console.WriteLine("Sent message: " + message);
+                }
+            }
+            catch (Exception ex)
+            {
+            }
         }
     }
 }
--- Azure.ReceiveEvents/Program.cs
+++ Azure.ReceiveEvents/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace Azure.ReceiveEvents
+﻿using System;
+using System.Text;
+using Microsoft.ServiceBus.Messaging;
+
+namespace Azure.ReceiveEvents
 {
     class Program
     {
         static void Main()
         {
+            try
+            {
+                var factory = MessagingFactory.CreateFromConnectionString("Endpoint=sb://optus-test.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=XmnjtQXdULTRKNm0sv8rNHCjTH+j0Iejx8p1gntJP74=" + ";TransportType=Amqp");
+                var client = factory.CreateEventHubClient("optus");
+                var group = client.GetDefaultConsumerGroup();
+                var receiver = group.CreateReceiver(client.GetRuntimeInformation().PartitionIds[0]);
+                bool receive = true;
+                while (receive)
+                {
+                    var message = receiver.Receive();
+                    Console.WriteLine("Received message: " + Encoding.UTF8.GetString(message.GetBytes()));
+                }
+            }
+            catch (Exception ex)
+            {
+            }
         }
     }
 }
