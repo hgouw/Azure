0e4e13648969a490f8dc29a58d6a0dc89670c0dc
Herman Gouw <hermangouw@gmail.com>
2016-11-28 15:09:15 +1100

    Implemented SendQueues

--- Azure.SendQueues/Program.cs
+++ Azure.SendQueues/Program.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.WindowsAzure;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Auth;
 using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Queue;
 
 namespace Azure.SendQueues
 {
@@ -14,18 +10,24 @@ namespace Azure.SendQueues
     {
         static void Main()
         {
-            var storageAccount = new CloudStorageAccount(new StorageCredentials(CloudConfigurationManager.GetSetting("AccountName"), CloudConfigurationManager.GetSetting("AccountKeyValue")), true);
+            Console.WriteLine("Started sending messages to Azure Queue Storage");
+            var storageAccount = new CloudStorageAccount(new StorageCredentials("hgouw", "lXr8WD/7ULcm9AIrEW9s0q2jdQaVYcTB9rSaaQZJ51m3VxZya+t9yfEpavg5OFuni/A6kYx9bLCdimNRq7roHg=="), true);
+            //var storageAccount = new CloudStorageAccount(new StorageCredentials(CloudConfigurationManager.GetSetting("AccountName"), CloudConfigurationManager.GetSetting("AccountKeyValue")), true);
             var blobClient = storageAccount.CreateCloudBlobClient();
             var container = blobClient.GetContainerReference("optus");
             var blobs = container.ListBlobs();
+            var queueClient = storageAccount.CreateCloudQueueClient();
+            var queue = queueClient.GetQueueReference("optus");
+            queue.CreateIfNotExists();
             foreach (var blob in blobs)
             {
                 if (blob is CloudBlockBlob)
                 {
-                    var blobName = ((CloudBlockBlob)blob).Name;
-                    output.Add(blobName);
+                    queue.AddMessage(new CloudQueueMessage(((CloudBlockBlob)blob).Name));
                 }
             }
+            Console.WriteLine("Successfully sending messages to Azure Queue Storage");
+            Console.ReadLine();
         }
     }
 }
