9f5f0d9641e6766a106cafbc5f0e125b96777b6f
Herman Gouw <hermangouw@gmail.com>
2016-11-28 13:56:08 +1100

    Added Azure.SendQueues project

---
+++ Azure.SendQueues/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace Azure.SendQueues
+{
+    class Program
+    {
+        static void Main()
+        {
+            var storageAccount = new CloudStorageAccount(new StorageCredentials(CloudConfigurationManager.GetSetting("AccountName"), CloudConfigurationManager.GetSetting("AccountKeyValue")), true);
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            var container = blobClient.GetContainerReference("optus");
+            var blobs = container.ListBlobs();
+            foreach (var blob in blobs)
+            {
+                if (blob is CloudBlockBlob)
+                {
+                    var blobName = ((CloudBlockBlob)blob).Name;
+                    output.Add(blobName);
+                }
+            }
+        }
+    }
+}
