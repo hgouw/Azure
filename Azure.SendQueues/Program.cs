14d5e738317571cd1676b27aa7e32c607b6e4460
Herman Gouw <hermangouw@gmail.com>
2016-12-01 11:50:03 +1100

    Refactored

--- Azure.SendQueues/Program.cs
+++ Azure.SendQueues/Program.cs
@@ -8,25 +8,34 @@ namespace Azure.SendQueues
 {
     class Program
     {
+        public static int i { get; set; }
+
         static void Main()
         {
             Console.WriteLine("Started sending messages to Azure Queue Storage");
             var storageAccount = new CloudStorageAccount(new StorageCredentials("hgouw", "lXr8WD/7ULcm9AIrEW9s0q2jdQaVYcTB9rSaaQZJ51m3VxZya+t9yfEpavg5OFuni/A6kYx9bLCdimNRq7roHg=="), true);
             //var storageAccount = new CloudStorageAccount(new StorageCredentials(CloudConfigurationManager.GetSetting("AccountName"), CloudConfigurationManager.GetSetting("AccountKeyValue")), true);
+
+            // Get the Blob Storage
             var blobClient = storageAccount.CreateCloudBlobClient();
             var container = blobClient.GetContainerReference("optus");
             var blobs = container.ListBlobs();
+
+            // Get the Queue Storage
             var queueClient = storageAccount.CreateCloudQueueClient();
             var queue = queueClient.GetQueueReference("optus");
             queue.CreateIfNotExists();
+
+            // For each blob send a message in the queue
             foreach (var blob in blobs)
             {
                 if (blob is CloudBlockBlob)
                 {
                     queue.AddMessage(new CloudQueueMessage(((CloudBlockBlob)blob).Name));
+                    i++;
                 }
             }
-            Console.WriteLine("Successfully sending messages to Azure Queue Storage");
+            Console.WriteLine($"Successfully sent {i} messages to Azure Queue Storage");
             Console.ReadLine();
         }
     }
